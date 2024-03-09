20b662ec1dfc2af263525d9e0710b331f0d7d33c
iscanderoff <scanderoff@gmail.com>
2024-03-09 21:58:32 +0800

    Event system checks

--- gomp.go
+++ gomp.go
@@ -2,24 +2,51 @@ package main
 
 import "C"
 import (
+	"fmt"
+	"log"
 	"unsafe"
+
+	"github.com/kodeyeen/gomp/event"
 )
+
+var eventDispatcher = event.NewDispatcher()
+
+func init() {
+	eventDispatcher.On(event.TypeGameModeInit, func(evt *gameModeInitEvent) {
+		log.Println("GAME MODE INITIALIZED")
+	})
+
+	eventDispatcher.On(event.TypePlayerConnect, func(evt *playerConnectEvent) {
+		plr := evt.Player
+
+		plr.SendMessage(
+			0x00FF0000,
+			fmt.Sprintf("Hello %s", plr.Name()),
+		)
+	})
+}
 
 func main() {}
 
-//export OnGameModeInit
-func OnGameModeInit() {
+//export onGameModeInit
+func onGameModeInit() {
 	comp = getComponent()
-	comp.init("./components/empty-template.dll")
+	comp.init("./components/Gomp.dll")
+
+	gm := &GameMode{}
+
+	event.Dispatch(eventDispatcher, event.TypeGameModeInit, &gameModeInitEvent{
+		GameMode: gm,
+	})
 }
 
-//export OnPlayerConnect
-func OnPlayerConnect(plrHandle unsafe.Pointer) {
+//export onPlayerConnect
+func onPlayerConnect(plrHandle unsafe.Pointer) {
 	plr := newPlayer(plrHandle, getComponent())
 
-	plr.SendMessage(0x00FF0000, "Hello!")
+	event.Dispatch(eventDispatcher, event.TypePlayerConnect, &playerConnectEvent{
+		Player: plr,
+	})
 }
 
 // go build -o test.dll -buildmode=c-shared
-// set GOARCH=386
-// set CGO_ENABLED=1
