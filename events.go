648e1971efb6322896ef8e96cb70bba65d56a396
iscanderoff <scanderoff@gmail.com>
2024-03-11 00:50:46 +0800

    Events

--- events.go
+++ events.go
@@ -1,6 +1,11 @@
 package main
 
 type DisconnectReason int
+type DialogResponse int
+type PlayerState int
+type BodyPart int
+type PlayerClickSource int
+type PlayerBulletHitType int
 
 const (
 	DisconnectReasonTimeout DisconnectReason = iota
@@ -10,27 +15,194 @@ const (
 	DisconnectReasonModeEnd
 )
 
-type gameModeInitEvent struct {
+const (
+	DialogResponseRight DialogResponse = iota
+	DialogResponseLeft
+)
+
+const (
+	PlayerStateNone PlayerState = iota
+	PlayerStateOnFoot
+	PlayerStateDriver
+	PlayerStatePassenger
+	PlayerStateExitVehicle
+	PlayerStateEnterVehicleDriver
+	PlayerStateEnterVehiclePassenger
+	PlayerStateWasted
+	PlayerStateSpawned
+	PlayerStateSpectating
+)
+
+const (
+	BodyPartTorso BodyPart = iota + 3
+	BodyPartGroin
+	BodyPartLeftArm
+	BodyPartRightArm
+	BodyPartLeftLeg
+	BodyPartRightLeg
+	BodyPartHead
+)
+
+const (
+	PlayerClickSourceScoreboard = iota
+)
+
+const (
+	PlayerBulletHitTypeNone PlayerBulletHitType = iota
+	PlayerBulletHitTypePlayer
+	PlayerBulletHitTypeVehicle
+	PlayerBulletHitTypeObject
+	PlayerBulletHitTypePlayerObject
+)
+
+type GameModeInitEvent struct {
 	GameMode *GameMode
+}
+
+type PlayerRequestSpawnEvent struct {
+	Player *Player
+}
+
+type PlayerSpawnEvent struct {
+	Player *Player
 }
 
 // Player connect events
 
-type incomingConnectionEvent struct {
+type IncomingConnectionEvent struct {
 	Player    *Player
 	IPAddress string
 	Port      int
 }
 
-type playerConnectEvent struct {
+type PlayerConnectEvent struct {
 	Player *Player
 }
 
-type playerDisconnectEvent struct {
+type PlayerDisconnectEvent struct {
 	Player *Player
 	Reason DisconnectReason
 }
 
-type playerClientInitEvent struct {
+type PlayerClientInitEvent struct {
 	Player *Player
+}
+
+type PlayerStreamInEvent struct {
+	Player    *Player
+	ForPlayer *Player
+}
+
+type PlayerStreamOutEvent struct {
+	Player    *Player
+	ForPlayer *Player
+}
+
+type PlayerTextEvent struct {
+	Player  *Player
+	Message string
+}
+
+type PlayerCommandTextEvent struct {
+	Player  *Player
+	Message string
+}
+
+type PlayerScoreChangeEvent struct {
+	Player *Player
+	Score  int
+}
+
+type PlayerNameChangeEvent struct {
+	Player  *Player
+	OldName string
+}
+
+type PlayerInteriorChangeEvent struct {
+	Player      *Player
+	NewInterior uint
+	OldInterior uint
+}
+
+type PlayerStateChangeEvent struct {
+	Player   *Player
+	NewState PlayerState
+	OldState PlayerState
+}
+
+type PlayerKeyStateChangeEvent struct {
+	Player  *Player
+	NewKeys uint
+	OldKeys uint
+}
+
+type PlayerDeathEvent struct {
+	Player *Player
+	Killer *Player
+	Reason int
+}
+
+type PlayerTakeDamageEvent struct {
+	Player *Player
+	From   *Player
+	Amount float32
+	Weapon uint
+	Part   BodyPart
+}
+
+type PlayerGiveDamageEvent struct {
+	Player *Player
+	To     *Player
+	Amount float32
+	Weapon uint
+	Part   BodyPart
+}
+
+type PlayerClickMapEvent struct {
+	Player   *Player
+	Position *Position
+}
+
+type PlayerClickPlayerEvent struct {
+	Player  *Player
+	Clicked *Player
+	Source  PlayerClickSource
+}
+
+type ClientCheckResponseEvent struct {
+	Player     *Player
+	ActionType int
+	Address    int
+	Results    int
+}
+
+type PlayerUpdateEvent struct {
+	Player *Player
+	Now    int
+}
+
+type DialogResponseEvent struct {
+	Player    *Player
+	DialogID  int
+	Response  DialogResponse
+	ListItem  int
+	InputText string
+}
+
+type PlayerGiveDamageActorEvent struct {
+	Player *Player
+	Actor  *Player
+	Amount float32
+	Weapon uint
+	Part   BodyPart
+}
+
+type ActorStreamInEvent struct {
+	Actor     *Player
+	ForPlayer *Player
+}
+
+type ActorStreamOutEvent struct {
+	Actor     *Player
+	ForPlayer *Player
 }
--- gomp.go
+++ gomp.go
@@ -13,11 +13,11 @@ import (
 var eventDispatcher = event.NewDispatcher()
 
 func init() {
-	eventDispatcher.On(event.TypeGameModeInit, func(evt *gameModeInitEvent) {
+	eventDispatcher.On(event.TypeGameModeInit, func(evt *GameModeInitEvent) {
 		log.Println("GAME MODE INITIALIZED")
 	})
 
-	eventDispatcher.On(event.TypePlayerConnect, func(evt *playerConnectEvent) {
+	eventDispatcher.On(event.TypePlayerConnect, func(evt *PlayerConnectEvent) {
 		plr := evt.Player
 
 		plr.SendMessage(
@@ -27,6 +27,10 @@ func init() {
 
 		NewVehicle(602, Position{2161.8389, -1143.7473, 24.6501}, 266.9070)
 	})
+}
+
+func On(evtType event.Type, callback any) {
+	eventDispatcher.On(evtType, callback)
 }
 
 func main() {}
@@ -42,41 +46,266 @@ func onGameModeInit() {
 
 	gm := &GameMode{}
 
-	event.Dispatch(eventDispatcher, event.TypeGameModeInit, &gameModeInitEvent{
+	event.Dispatch(eventDispatcher, event.TypeGameModeInit, &GameModeInitEvent{
 		GameMode: gm,
+	})
+}
+
+// Player spawn events
+
+//export onPlayerRequestSpawn
+func onPlayerRequestSpawn(player unsafe.Pointer) {
+	event.Dispatch(eventDispatcher, event.TypeIncomingConnection, &PlayerRequestSpawnEvent{
+		Player: &Player{player},
+	})
+}
+
+//export onPlayerSpawn
+func onPlayerSpawn(player unsafe.Pointer) {
+	event.Dispatch(eventDispatcher, event.TypeIncomingConnection, &PlayerSpawnEvent{
+		Player: &Player{player},
 	})
 }
 
 // Player connect events
 
 //export onIncomingConnection
-func onIncomingConnection(plrHandle unsafe.Pointer, ipAddress *C.char, port C.ushort) {
-	event.Dispatch(eventDispatcher, event.TypeIncomingConnection, &incomingConnectionEvent{
-		Player:    &Player{plrHandle},
+func onIncomingConnection(player unsafe.Pointer, ipAddress *C.char, port C.ushort) {
+	event.Dispatch(eventDispatcher, event.TypeIncomingConnection, &IncomingConnectionEvent{
+		Player:    &Player{player},
 		IPAddress: C.GoString(ipAddress),
 		Port:      int(port),
 	})
 }
 
 //export onPlayerConnect
-func onPlayerConnect(plrHandle unsafe.Pointer) {
-	event.Dispatch(eventDispatcher, event.TypePlayerConnect, &playerConnectEvent{
-		Player: &Player{plrHandle},
+func onPlayerConnect(player unsafe.Pointer) {
+	event.Dispatch(eventDispatcher, event.TypePlayerConnect, &PlayerConnectEvent{
+		Player: &Player{player},
 	})
 }
 
 //export onPlayerDisconnect
-func onPlayerDisconnect(plrHandle unsafe.Pointer, reason int) {
-	event.Dispatch(eventDispatcher, event.TypePlayerDisconnect, &playerDisconnectEvent{
-		Player: &Player{plrHandle},
+func onPlayerDisconnect(player unsafe.Pointer, reason int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerDisconnect, &PlayerDisconnectEvent{
+		Player: &Player{player},
 		Reason: DisconnectReason(reason),
 	})
 }
 
 //export onPlayerClientInit
-func onPlayerClientInit(plrHandle unsafe.Pointer) {
-	event.Dispatch(eventDispatcher, event.TypePlayerClientInit, &playerClientInitEvent{
-		Player: &Player{plrHandle},
+func onPlayerClientInit(player unsafe.Pointer) {
+	event.Dispatch(eventDispatcher, event.TypePlayerClientInit, &PlayerClientInitEvent{
+		Player: &Player{player},
+	})
+}
+
+// Player stream events
+
+//export onPlayerStreamIn
+func onPlayerStreamIn(player unsafe.Pointer, forPlayer unsafe.Pointer) {
+	event.Dispatch(eventDispatcher, event.TypePlayerStreamIn, &PlayerStreamInEvent{
+		Player:    &Player{player},
+		ForPlayer: &Player{forPlayer},
+	})
+}
+
+//export onPlayerStreamOut
+func onPlayerStreamOut(player unsafe.Pointer, forPlayer unsafe.Pointer) {
+	event.Dispatch(eventDispatcher, event.TypePlayerStreamOut, &PlayerStreamOutEvent{
+		Player:    &Player{player},
+		ForPlayer: &Player{forPlayer},
+	})
+}
+
+// Player text events
+
+//export onPlayerText
+func onPlayerText(player unsafe.Pointer, message *C.char) {
+	event.Dispatch(eventDispatcher, event.TypePlayerText, &PlayerTextEvent{
+		Player:  &Player{player},
+		Message: C.GoString(message),
+	})
+}
+
+//export onPlayerCommandText
+func onPlayerCommandText(player unsafe.Pointer, message *C.char) {
+	event.Dispatch(eventDispatcher, event.TypePlayerCommandText, &PlayerCommandTextEvent{
+		Player:  &Player{player},
+		Message: C.GoString(message),
+	})
+}
+
+// Player shot events
+
+// TODO:
+//
+////export onPlayerWeaponShot
+// func onPlayerWeaponShot(player unsafe.Pointer, weapon C.uchar, hitType C.int, hitID C.ushort) {
+// 	event.Dispatch(eventDispatcher, event.TypePlayerCommandText, &PlayerCommandTextEvent{
+// 		Player:  &Player{player},
+// 		Message: C.GoString(message),
+// 	})
+// }
+
+// Player data change events
+
+//export onPlayerScoreChange
+func onPlayerScoreChange(player unsafe.Pointer, score C.int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerScoreChange, &PlayerScoreChangeEvent{
+		Player: &Player{player},
+		Score:  int(score),
+	})
+}
+
+//export onPlayerNameChange
+func onPlayerNameChange(player unsafe.Pointer, oldName *C.char) {
+	event.Dispatch(eventDispatcher, event.TypePlayerNameChange, &PlayerNameChangeEvent{
+		Player:  &Player{player},
+		OldName: C.GoString(oldName),
+	})
+}
+
+//export onPlayerInteriorChange
+func onPlayerInteriorChange(player unsafe.Pointer, newInterior C.uint, oldInterior C.uint) {
+	event.Dispatch(eventDispatcher, event.TypePlayerInteriorChange, &PlayerInteriorChangeEvent{
+		Player:      &Player{player},
+		NewInterior: uint(newInterior),
+		OldInterior: uint(oldInterior),
+	})
+}
+
+//export onPlayerStateChange
+func onPlayerStateChange(player unsafe.Pointer, newState C.int, oldState C.int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerStateChange, &PlayerStateChangeEvent{
+		Player:   &Player{player},
+		NewState: PlayerState(newState),
+		OldState: PlayerState(oldState),
+	})
+}
+
+//export onPlayerKeyStateChange
+func onPlayerKeyStateChange(player unsafe.Pointer, newKeys C.uint, oldKeys C.uint) {
+	event.Dispatch(eventDispatcher, event.TypePlayerKeyStateChange, &PlayerKeyStateChangeEvent{
+		Player:  &Player{player},
+		NewKeys: uint(newKeys),
+		OldKeys: uint(oldKeys),
+	})
+}
+
+// Player death and damage events
+
+//export onPlayerDeath
+func onPlayerDeath(player unsafe.Pointer, killer unsafe.Pointer, reason C.int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerDeath, &PlayerDeathEvent{
+		Player: &Player{player},
+		Killer: &Player{killer},
+		Reason: int(reason),
+	})
+}
+
+//export onPlayerTakeDamage
+func onPlayerTakeDamage(player unsafe.Pointer, from unsafe.Pointer, amount C.float, weapon C.uint, part C.int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerTakeDamage, &PlayerTakeDamageEvent{
+		Player: &Player{player},
+		From:   &Player{from},
+		Amount: float32(amount),
+		Weapon: uint(weapon),
+		Part:   BodyPart(part),
+	})
+}
+
+//export onPlayerGiveDamage
+func onPlayerGiveDamage(player unsafe.Pointer, to unsafe.Pointer, amount C.float, weapon C.uint, part C.int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerGiveDamage, &PlayerGiveDamageEvent{
+		Player: &Player{player},
+		To:     &Player{to},
+		Amount: float32(amount),
+		Weapon: uint(weapon),
+		Part:   BodyPart(part),
+	})
+}
+
+// Player click events
+
+//export onPlayerClickMap
+func onPlayerClickMap(player unsafe.Pointer, x, y, z C.float) {
+	event.Dispatch(eventDispatcher, event.TypePlayerClickMap, &PlayerClickMapEvent{
+		Player:   &Player{player},
+		Position: &Position{float32(x), float32(y), float32(z)},
+	})
+}
+
+//export onPlayerClickPlayer
+func onPlayerClickPlayer(player unsafe.Pointer, clicked unsafe.Pointer, source C.int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerClickPlayer, &PlayerClickPlayerEvent{
+		Player:  &Player{player},
+		Clicked: &Player{clicked},
+		Source:  PlayerClickSource(source),
+	})
+}
+
+// Player client check event
+
+//export onClientCheckResponse
+func onClientCheckResponse(player unsafe.Pointer, actionType, address, results C.int) {
+	event.Dispatch(eventDispatcher, event.TypeClientCheckResponse, &ClientCheckResponseEvent{
+		Player:     &Player{player},
+		ActionType: int(actionType),
+		Address:    int(address),
+		Results:    int(results),
+	})
+}
+
+// Player update event
+
+//export onPlayerUpdate
+func onPlayerUpdate(player unsafe.Pointer, now C.int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerUpdate, &PlayerUpdateEvent{
+		Player: &Player{player},
+		Now:    int(now),
+	})
+}
+
+// Player dialog events
+
+//export onDialogResponse
+func onDialogResponse(player unsafe.Pointer, dialogID C.int, response C.int, listItem C.int, inputText *C.char) {
+	event.Dispatch(eventDispatcher, event.TypeDialogResponse, &DialogResponseEvent{
+		Player:    &Player{player},
+		DialogID:  int(dialogID),
+		Response:  DialogResponse(response),
+		ListItem:  int(listItem),
+		InputText: C.GoString(inputText),
+	})
+}
+
+// Actor events
+
+//export onPlayerGiveDamageActor
+func onPlayerGiveDamageActor(player unsafe.Pointer, actor unsafe.Pointer, amount C.float, weapon C.uint, part C.int) {
+	event.Dispatch(eventDispatcher, event.TypePlayerGiveDamageActor, &PlayerGiveDamageActorEvent{
+		Player: &Player{player},
+		Actor:  &Player{actor},
+		Amount: float32(amount),
+		Weapon: uint(weapon),
+		Part:   BodyPart(part),
+	})
+}
+
+//export onActorStreamIn
+func onActorStreamIn(actor unsafe.Pointer, player unsafe.Pointer) {
+	event.Dispatch(eventDispatcher, event.TypeActorStreamIn, &ActorStreamInEvent{
+		Actor:     &Player{actor},
+		ForPlayer: &Player{player},
+	})
+}
+
+//export onActorStreamOut
+func onActorStreamOut(actor unsafe.Pointer, player unsafe.Pointer) {
+	event.Dispatch(eventDispatcher, event.TypeActorStreamOut, &ActorStreamOutEvent{
+		Actor:     &Player{actor},
+		ForPlayer: &Player{player},
 	})
 }
 
