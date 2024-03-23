441d42139fbc1c73933ab271866a8583f97e39e3
iscanderoff <scanderoff@gmail.com>
2024-03-23 20:00:16 +0800

    Indentation

--- include/player.h
+++ include/player.h
@@ -5,12 +5,12 @@ extern "C"
 {
 #endif
 
-int player_getID(void* player);
-String player_getName(void* player);
-int player_setName(void* player, String name);
-Vector3 player_getPosition(void* player);
-void player_sendClientMessage(void* player, int colour, String message);
-void* player_getVehicle(void* player);
+    int player_getID(void* player);
+    String player_getName(void* player);
+    int player_setName(void* player, String name);
+    Vector3 player_getPosition(void* player);
+    void player_sendClientMessage(void* player, int colour, String message);
+    void* player_getVehicle(void* player);
 
 #ifdef __cplusplus
 }
