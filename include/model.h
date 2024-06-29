c2f7cd8a12313b4fc223760a9382845387ed728d
iscanderoff <scanderoff@gmail.com>
2024-06-29 16:58:36 +0800

    Add model_isValid function declaration

--- include/model.h
+++ include/model.h
@@ -6,6 +6,7 @@ extern "C" {
 
 	unsigned char model_add(uint8_t type, int32_t id, int32_t baseId, String dffName, String txdName, int32_t virtualWorld, uint8_t timeOn, uint8_t timeOff);
     String model_getNameFromCheckSum(uint32_t crc);
+    unsigned char model_isValid(int32_t modelId);
     unsigned char model_getPath(int32_t modelId, String* dffPath, String* txdPath);
 
 #ifdef __cplusplus
