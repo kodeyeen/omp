3aae54205c42e2e92cb61924ecd408544c6b6507
iscanderoff <scanderoff@gmail.com>
2024-06-29 16:50:27 +0800

    Fix custom model function calls

--- model.cpp
+++ model.cpp
@@ -5,15 +5,19 @@ extern "C" {
 #endif
 
     unsigned char model_add(uint8_t type, int32_t id, int32_t baseId, String dffName, String txdName, int32_t virtualWorld, uint8_t timeOn, uint8_t timeOff) {
-        return call<unsigned char>("model_add", type, id, baseId, dffName, txdName, virtualWorld, timeOn, timeOff);
+        return call<unsigned char>("customModel_add", type, id, baseId, dffName, txdName, virtualWorld, timeOn, timeOff);
     }
 
-    String model_getNameFromCheckSum(uint32_t crc) {
-        return call<String>("model_getNameFromCheckSum", crc);
+    String model_getNameFromCheckSum(uint32_t checksum) {
+        return call<String>("customModel_getNameFromCheckSum", checksum);
+    }
+
+    unsigned char model_isValid(int32_t modelId) {
+        return call<unsigned char>("customModel_isValid", modelId);
     }
 
     unsigned char model_getPath(int32_t modelId, String* dffPath, String* txdPath) {
-        return call<unsigned char>("model_getPath", modelId, dffPath, txdPath);
+        return call<unsigned char>("customModel_getPath", modelId, dffPath, txdPath);
     }
 
 #ifdef __cplusplus
