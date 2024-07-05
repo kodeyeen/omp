5be814d0f26b01e5724110c31d183f284d01bcfd
iscanderoff <scanderoff@gmail.com>
2024-07-05 14:58:26 +0800

    Remove C wrapper files. Will generate them from capi/apidocs/api.json

--- component.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/component.h"
-
-extern "C" {
-    void* Component_Create(uint64_t uid, const char* name, struct ComponentVersion version, void* onReady, void* onReset, void* onFree) {
-        return call<void*>("Component_Create", uid, name, version, onReady, onReset, onFree);
-    }
-}
